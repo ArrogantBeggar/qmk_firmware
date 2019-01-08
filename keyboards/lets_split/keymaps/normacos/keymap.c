a3d5f0a11e4373debc92b2b1c86c55ba2d33c7c2
Jorge Porras <jorgeporras.tij@gmail.com>
2019-01-08 15:37:39 -0800

    fixed misplaced function keys, removed unused macros

--- keyboards/lets_split/keymaps/normacos/keymap.c
+++ keyboards/lets_split/keymaps/normacos/keymap.c
@@ -22,7 +22,6 @@ enum custom_keycodes {
   MIN,
   OUTLOOK,
   TERM,
-  SLACK,
   SPOT,
   TEAMS
 };
@@ -71,21 +70,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
  * | Shift|  F9  |  F10 |  F11 |  F12 |      | |   /  |   1  |   2  |   3  |   -  |SftEnt|
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * | Lock | Home |  End |  Alt | Lower|  GUI | | Space| Raise|   0  |   .  | Spot |Chrome|
+ * | Lock | Home |  End |  Alt | Lower|  GUI | | Space| Raise|   0  |   .  | Spot |      |
  * `-----------------------------------------' `-----------------------------------------'
  */
 [_LOWER] = LAYOUT_ortho_4x12( \
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   XXXXXXX, KC_NLCK, KC_P7,   KC_P8, KC_P9,   KC_PEQL, _______, \
-  _______, KC_F5,   KC_F6,   KC_F8,   KC_F9,   XXXXXXX, KC_PAST, KC_P4,   KC_P5, KC_P6,   KC_PPLS, KC_DEL,  \
+  _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX, KC_PAST, KC_P4,   KC_P5, KC_P6,   KC_PPLS, KC_DEL,  \
   _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, KC_PSLS, KC_P1,   KC_P2, KC_P3,   KC_PMNS, _______, \
-  SCN_LCK, _______, _______, _______, _______, _______, _______, _______, KC_P0, KC_PDOT, SPOT,    CHROME
+  SCN_LCK, _______, _______, _______, _______, _______, _______, _______, KC_P0, KC_PDOT, SPOT,    XXXXXXX
 ),
 
 /* Raise
  * ,-----------------------------------------. ,-----------------------------------------.
  * |  Tab |   !  |   @  |   #  |   $  |   %  | |   ^  |   &  |   *  |   (  |   )  |  Del |
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * |CtlEsc|  Log | Teams| Slack| Term | OutLk| |   =  |   -  |   \  |   [  |   ]  |   `  |
+ * |CtlEsc|  Log | Teams|Chrome| Term | OutLk| |   =  |   -  |   \  |   [  |   ]  |   `  |
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
  * | Shift|ClrHom|ClrEnd|ShHome| ShEnd|Search| | Play | Mute | VolDn| VolUp|ScnSht|SftEnt|
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
@@ -94,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_ortho_4x12( \
   _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  \
-  _______, LOGW,    TEAMS,   SLACK,   TERM,    OUTLOOK, KC_EQL,  KC_MINS, KC_BSLS, KC_LBRC, KC_RBRC, KC_GRV,  \
+  _______, LOGW,    TEAMS,   CHROME,  TERM,    OUTLOOK, KC_EQL,  KC_MINS, KC_BSLS, KC_LBRC, KC_RBRC, KC_GRV,  \
   _______, SH_CLRH, SH_CLRE, SH_HOME, SH_END,  SH_RSCH, KC_MPLY, KC_MUTE, KC_VOLD, KC_VOLU, SCN_SHT, _______, \
   SCN_LCK, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
@@ -162,13 +161,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         SEND_STRING(SS_LGUI(SS_TAP(X_SPACE)));
         _delay_ms(100);
         SEND_STRING("Microsoft Outlook");
-        _delay_ms(100);
-        SEND_STRING(SS_TAP(X_ENTER));
-        return false;
-      case SLACK:  // Open Slack
-        SEND_STRING(SS_LGUI(SS_TAP(X_SPACE)));
-        _delay_ms(100);
-        SEND_STRING("Slack");
         _delay_ms(100);
         SEND_STRING(SS_TAP(X_ENTER));
         return false;
